fe098fb7e80483158653324166dd01c00a0eef91
PedroLaudo <pedro2004laudo@gmail.com>
2025-01-15 14:45:07 +0000

    tenta

--- projeto/src/components/Navbar/Navbar.jsx
+++ projeto/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react';
+
 import './Navbar.css'
 
 
 const Navbar = () => {
+    const [menu,setMenu] = useState("Página Inicial");
   return (
     <div className='navbar'>
-       <img src="\src\assets\bola_champions.png" alt="Champions League Ball" />
+       <img src="\src\assets\bola_champions.png" alt="Champions League Ball" className='logo' />
 
         <ul className="navbar-menu">
-            <li>Página Inicial</li>
-            <li>Acessórios</li>
-            <li>Chuteiras</li>
-            <li>Camisolas</li>
+            <li onClick={()=>setMenu("Página Inicial")} className={menu==="Página Inicial"?"active":""} >Página Inicial</li>
+            <li onClick={()=>setMenu("Acessórios")} className={menu==="Acessórios"?"active":""}>Acessórios</li>
+            <li onClick={()=>setMenu("Chuteiras")} className={menu==="Chuteiras"?"active":""}>Chuteiras</li>
+            <li onClick={()=>setMenu("Camisolas")} className={menu==="Camisolas"?"active":""}>Camisolas</li>
         </ul>
         <div className="navbar-right">
-            <img src="\src\assets\1490793870-user-interface25_82355.webp" alt="Pesquisa" />
+            <img src="\src\assets\1490793870-user-interface25_82355.webp" alt="Pesquisa" className='lupa' />
             <div className="navbar-search-icon"></div>
             <img src="\src\assets\126510-removebg-preview.png" alt="Pesquisa" />
             <div className="dot"></div>    
