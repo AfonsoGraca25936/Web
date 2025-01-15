b19bf5049888267a28eefe18740795829838c811
PedroLaudo <pedro2004laudo@gmail.com>
2025-01-15 15:30:20 +0000

    1

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom'; 
+import PaginaInicial from './Paginas/PáginaInicial/PaginaInicial';
+import Carrinho from './Paginas/Carrinho/Carrinho';
+import Compras from './Paginas/Compras/Compras';
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+       
+        <Route path="/" element={<PaginaInicial />} /> {/* Definir a rota para a página inicial */}
+        <Route path="/acessorios" element={<Carrinho />} />
+        <Route path="/chuteiras" element={<Compras />} />  
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
