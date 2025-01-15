c001feae7250babab6919e56925712e52ab3873b
PedroLaudo <pedro2004laudo@gmail.com>
2025-01-15 13:37:38 +0000

    inserir imagens

---
+++ projeto/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+const Navbar = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Navbar
